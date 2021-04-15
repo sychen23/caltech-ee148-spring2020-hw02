9dabebb6a54231a084e63994d1987c54cd3b7de9
Sharon Chen <sychen23@yahoo.com>
2021-04-14 21:28:43 -0700

    modified run_predictions.py

--- run_predictions.py
+++ run_predictions.py
@@ -1,7 +1,9 @@
 import os
+from tqdm import tqdm
 import numpy as np
 import json
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def compute_convolution(I, T, stride=None):
     '''
@@ -15,7 +17,26 @@ def compute_convolution(I, T, stride=None):
     '''
     BEGIN YOUR CODE
     '''
-    heatmap = np.random.random((n_rows, n_cols))
+
+    heatmap = np.zeros((n_rows, n_cols))
+    T_height, T_width = np.shape(T)
+    for i in tqdm(range(n_rows-T_height)):
+        if i % 50 == 0:
+            plt.imshow(heatmap)
+            plt.savefig('%d-saved_figure.png' % i)
+
+        for j in range(n_cols-T_width):
+            test_I = I[i:i+T_height,j:j+T_width]
+            for test_i in range(T_height):
+                for test_j in range(T_width):
+                    test_T = np.ones(3)*T[test_i, test_j]
+                    dist = np.dot(test_I[test_i, test_j], test_T)
+                    heatmap_i = i + T_height // 2
+                    heatmap_j = j + T_width // 2
+                    heatmap[heatmap_i, heatmap_j] = np.arccos(dist) / np.pi
+
+    plt.imshow(heatmap)
+    plt.savefig('heatmap.png') 
 
     '''
     END YOUR CODE
@@ -36,27 +57,24 @@ def predict_boxes(heatmap):
     BEGIN YOUR CODE
     '''
     
-    '''
-    As an example, here's code that generates between 1 and 5 random boxes
-    of fixed size and returns the results in the proper format.
-    '''
+    (n_rows,n_cols) = np.shape(heatmap)
+    box_dims = [(100, 50), (20, 10), (10, 5)]
+    threshold = 10
 
-    box_height = 8
-    box_width = 6
-
-    num_boxes = np.random.randint(1,5)
-
-    for i in range(num_boxes):
-        (n_rows,n_cols,n_channels) = np.shape(I)
-
-        tl_row = np.random.randint(n_rows - box_height)
-        tl_col = np.random.randint(n_cols - box_width)
-        br_row = tl_row + box_height
-        br_col = tl_col + box_width
-
-        score = np.random.random()
-
-        output.append([tl_row,tl_col,br_row,br_col, score])
+    for box_dim in box_dims:
+        box_height, box_width = box_dim
+        for i in range(n_rows - box_height):
+            for j in range(n_cols - box_width):
+                candidate_box = heatmap[i:i+box_height, j:j+box_width]
+                score = np.mean(candidate_box)
+                if score > threshold:
+                    tl_row = i
+                    tl_col = j
+                    br_row = i + box_height
+                    br_col = j + box_width
+                    output.append([tl_row,tl_col,br_row,br_col, score])
+    plt.imshow(heatmap)
+    plt.savefig('heatmap.png') 
 
     '''
     END YOUR CODE
@@ -64,6 +82,23 @@ def predict_boxes(heatmap):
 
     return output
 
+def show_img(I, img_name, rect_list):
+    # Create figure and axes
+    fig, ax = plt.subplots()
+    fig.set_figwidth(5)
+    fig.set_figheight(5)
+    # Display the image
+    ax.imshow(I)
+    for rect in rect_list:
+        tl_row, tl_col, br_row, br_col = rect
+        # Create a Rectangle patch
+        rect = patches.Rectangle((tl_col-1, tl_row-1), br_col-tl_col, br_row-tl_row,
+                                 linewidth=1, edgecolor='r', facecolor='none')
+        
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+    plt.title(img_name)
+    plt.savefig(img_name)
 
 def detect_red_light_mf(I):
     '''
@@ -91,7 +126,9 @@ def detect_red_light_mf(I):
     T = np.random.random((template_height, template_width))
 
     heatmap = compute_convolution(I, T)
+    print('computed heatmap')
     output = predict_boxes(heatmap)
+    print('computed output')
 
     '''
     END YOUR CODE
@@ -103,17 +140,19 @@ def detect_red_light_mf(I):
 
     return output
 
+user_profile = os.environ['HOME']
+
 # Note that you are not allowed to use test data for training.
 # set the path to the downloaded data:
-data_path = '../data/RedLights2011_Medium'
+data_path = '%s/data/EE148/RedLights2011_Medium' % user_profile
 
 # load splits: 
-split_path = '../data/hw02_splits'
+split_path = '%s/data/EE148/hw02_splits' % user_profile
 file_names_train = np.load(os.path.join(split_path,'file_names_train.npy'))
-file_names_test = np.load(os.path.join(split_Path,'file_names_test.npy'))
+file_names_test = np.load(os.path.join(split_path,'file_names_test.npy'))
 
 # set a path for saving predictions:
-preds_path = '../data/hw02_preds'
+preds_path = '%s/data/EE148/hw02_preds' % user_profile
 os.makedirs(preds_path, exist_ok=True) # create directory if needed
 
 # Set this parameter to True when you're done with algorithm development:
@@ -132,6 +171,7 @@ for i in range(len(file_names_train)):
     I = np.asarray(I)
 
     preds_train[file_names_train[i]] = detect_red_light_mf(I)
+    print(preds_train[file_names_train[i]])
 
 # save preds (overwrites any previous predictions!)
 with open(os.path.join(preds_path,'preds_train.json'),'w') as f:
